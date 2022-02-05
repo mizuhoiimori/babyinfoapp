c02f04fa95cf6fc43b2b488ba7777053ff6b1d63
mizuhoiimori <tanaka.mizuho.com@gmail.com>
2022-02-05 06:55:41 +0000

    search

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,12 +9,7 @@ class ApplicationController < ActionController::Base
   def after_sign_out_path_for(resource)
     root_path
   end
-
-  # def search
-  #   @search = Item.ransack(params[:q])
-  #   @items = @search.result
-  # end
-
+  
   protected
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email])
